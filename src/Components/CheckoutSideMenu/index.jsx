3bc73bed1e6e63d0f78543d8a6222c458c8185d4
juansilva <juandsr@hotmail.com>
2023-07-21 15:03:32 -0400

    adding order items to checkout

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
+import OrderCard from '../../Components/OrderCard'
 import './styles.css'
 
 const CheckoutSideMenu = () => {
@@ -9,13 +10,26 @@ const CheckoutSideMenu = () => {
     return (
         <aside className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white`}>
             <div className='flex justify-between items-center p-6'>
-                <h2 className='font-medium text-xl'>Detail</h2>
+                <h2 className='font-medium text-xl'>Checkout</h2>
                 <div>
                     <XMarkIcon
                         className='h-6 w-6 text-black cursor-pointer'
                         onClick={() => context.closeCheckoutSideMenu()}
                     ></XMarkIcon>
                 </div>
+            </div>
+            <div className='px-6 overflow-y-scroll'>
+                {
+                    context.cartProducts.map(product => (
+                        <OrderCard
+                            key={product.id}
+                            title={product.title}
+                            imageUrl={product.images[0]}
+                            price={product.price}
+                        />
+                    )
+                    )
+                }
             </div>
         </aside>
     )
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,6 +3,7 @@ import { createContext, useState } from "react";
 export const ShoppingCartContext = createContext();
 
 export const ShoppingCartProvider = ({ children }) => {
+
     const [count, setCount] = useState(0)
 
     const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
